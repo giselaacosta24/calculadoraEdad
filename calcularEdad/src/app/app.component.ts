e2dc833f4f3628790fb5aab78f94e6c6d53461a7
giselaacosta24 <giseacosta651@gmail.com>
2022-03-31 19:47:05 -0300

    agrego logica

--- calcularEdad/src/app/app.component.ts
+++ calcularEdad/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calcularEdad';
+  public number1 :any;
+  public number2 : any;
+  public result1 : any;
+  public result2 : any;
+
+  constructor(private location: Location) {}
+  public calc(){
+    this.result1 = (this.number1 + this.number2)/2
+    this.result2 = this.number1 + this.number2
+
+  }
+
+  public limpiar(){
+    location.reload();
+
+
+  }
 }
