02e6eb66acfe819cdd74c55f4db99d4be1bd91b0
giselaacosta24 <giseacosta651@gmail.com>
2022-03-31 23:32:33 -0300

    agrego clase suma

---
+++ calcularEdad/src/app/Entidades/suma.ts
@@ -0,0 +1,13 @@
+export class Suma {
+    num1 :number=0;
+    num2 :number=0;
+    resultado :number=0;
+    resultado2 :number=0;
+
+    public sumar()
+    {
+     this.resultado=this.num1+this.num2;
+     console.log(this.resultado);
+    }
+    }
+    
--- calcularEdad/src/app/app.component.ts
+++ calcularEdad/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-
+import { Suma} from '../app/Entidades/suma';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,15 +8,16 @@ import { Location } from '@angular/common';
 })
 export class AppComponent {
   title = 'calcularEdad';
-  public number1 :number=0;
-  public number2 : number=0;
-  public result1 :number=0;
-  public result2 : number=0;
+  // public number1 :number=0;
+  // public number2 : number=0;
+  // public result1 :number=0;
+  // public result2 : number=0;
+  miSuma=new Suma();
 
   constructor(private location: Location) {}
   public calc(){
-    this.result1 = (this.number1 + this.number2)/2
-    this.result2 = this.number1 + this.number2
+    this.miSuma.resultado = (this.miSuma.num1 + this.miSuma.num2)/2
+    this.miSuma.resultado2 = this.miSuma.num1  + this.miSuma.num2
 
   }
 
